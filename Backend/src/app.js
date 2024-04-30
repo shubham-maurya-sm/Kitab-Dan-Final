e05ce7a56b2b0f6058c02420c7ef777327eecc09
Shubham Maurya <160427804+shubham-maurya-sm@users.noreply.github.com>
2024-04-30 21:12:04 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ dotenv.config({
 const app = express()
 
 const __dirname = path.resolve();
-app.use(cors({ origin: 'https://kitab-dan.vercel.app' })); 
+app.use(cors({ origin: 'https://kitab-dan.vercel.app/' })); 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
